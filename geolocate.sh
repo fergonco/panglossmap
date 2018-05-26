7bc6f43c0fbb6e302450b97a14d7b4c95bf7ac07
Fernando González Cortés <fernando.gonzalez@geomati.co>
2018-05-26 09:29:03 +0200

    geolocate script

---
+++ geolocate.sh
@@ -0,0 +1,10 @@
+set -e
+
+while read -r var
+do
+  wget -q -O /tmp/a "https://maps.googleapis.com/maps/api/geocode/json?region=ch&address=$var"
+  lat=$(cat /tmp/a | jq -r '.results[0].geometry.location.lat')
+  lon=$(cat /tmp/a | jq -r '.results[0].geometry.location.lng')
+  echo $lat,$lon
+done
+
