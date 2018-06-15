198c59f3e589705c5f6925a5c710168d4761200a
Fernando González Cortés <fernando.gonzalez@geomati.co>
2018-06-15 14:23:06 +0200

    geolocate input data

--- geolocate.sh
+++ geolocate.sh
@@ -5,6 +5,6 @@ do
   wget -q -O /tmp/a "https://maps.googleapis.com/maps/api/geocode/json?region=ch&address=$var"
   lat=$(cat /tmp/a | jq -r '.results[0].geometry.location.lat')
   lon=$(cat /tmp/a | jq -r '.results[0].geometry.location.lng')
-  echo $lat,$lon
+  echo $var,$lat,$lon
 done
 
